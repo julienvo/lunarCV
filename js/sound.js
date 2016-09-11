8d1b499a1293daa60ade30752af40448b4f02568
julienvo <julienvonau@gmail.com>
2016-09-11 12:54:36 +0200

    Added sound button

--- js/sound.js
+++ js/sound.js
@@ -1,3 +1,18 @@
 var son = new Audio('audio/thrust.mp3');
 son.volume = .3;
 son.loop = true;
+
+window.addEventListener('load',function(){
+  document.querySelector('#soundButton').addEventListener('click', function(){
+    console.log(son.muted)
+    if(son.muted){
+      this.innerHTML = '<span class="icon-volume-up"></span>';
+      son.muted = false;
+    }
+    else{
+      this.innerHTML = '<span class="icon-volume-off"></span>';
+      son.muted = true;
+    }
+  });
+
+});
